7ceee2f06f521b83322696998d8ea9a714661a70
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-08-24 18:54:30 +0200

    Add Match type

---
+++ src/models/match.models.ts
@@ -0,0 +1,34 @@
+export type Match =
+  | {
+      sport: "soccer";
+      participant1: string;
+      participant2: string;
+      score: string;
+    }
+  | {
+      sport: "volleyball";
+      participant1: string;
+      participant2: string;
+      score: string;
+    }
+  | {
+      sport: "handball";
+      participant1: string;
+      participant2: string;
+      score: string;
+    }
+  | {
+      sport: "basketball";
+      participant1: string;
+      participant2: string;
+      score: string[][];
+    }
+  | {
+      sport: "tennis";
+      participant1: string;
+      participant2: string;
+      score: string;
+    }
+  | {
+      sport: "ski jumping";
+    };
