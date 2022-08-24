9659e62c3e9c1794af9e87c93468ebd1d1999222
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-08-24 20:54:55 +0200

    Refactor basketball score formatting

--- src/data/match.data.ts
+++ src/data/match.data.ts
@@ -16,43 +16,39 @@ export class EventParser {
   }
 
   static formatScore(match: MatchResponseDTO) {
-    if (match.sport === "soccer" || match.sport === "handball") {
-      return match.score;
-    } else if (match.sport === "tennis" || match.sport === "volleyball") {
-      var scores =
-        /([0-9]+\:[0-9]+),([0-9]+\:[0-9]+),([0-9]+\:[0-9]+),([0-9]+\:[0-9]+)/.exec(
-          match.score
-        );
-      var set1 = scores![2];
-      var set2 = scores![3];
-      var set3 = scores![4];
+    switch (match.sport) {
+      case "soccer":
+      case "handball":
+        return match.score;
+      case "basketball":
+        return match.score.flat().join(",");
+      case "tennis":
+      case "volleyball": {
+        var scores =
+          /([0-9]+\:[0-9]+),([0-9]+\:[0-9]+),([0-9]+\:[0-9]+),([0-9]+\:[0-9]+)/.exec(
+            match.score
+          );
+        var set1 = scores![2];
+        var set2 = scores![3];
+        var set3 = scores![4];
 
-      return (
-        "Main score: " +
-        scores![1] +
-        " (" +
-        "set1 " +
-        set1 +
-        ", " +
-        "set2 " +
-        set2 +
-        ", " +
-        "set3 " +
-        set3 +
-        ")"
-      );
-    } else if (match.sport === "basketball") {
-      return (
-        match.score[0][0] +
-        "," +
-        match.score[0][1] +
-        "," +
-        match.score[1][0] +
-        "," +
-        match.score[1][1]
-      );
-    } else {
-      return "Exception: invalid sport";
+        return (
+          "Main score: " +
+          scores![1] +
+          " (" +
+          "set1 " +
+          set1 +
+          ", " +
+          "set2 " +
+          set2 +
+          ", " +
+          "set3 " +
+          set3 +
+          ")"
+        );
+      }
+      default:
+        return "Exception: invalid sport";
     }
   }
 }
