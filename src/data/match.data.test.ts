5ce0c4f6236c7bdc2546805378ba9c196ef5bb00
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-08-24 20:36:05 +0200

    Add test cases for EventParser

--- src/data/match.data.test.ts
+++ src/data/match.data.test.ts
@@ -1,5 +1,118 @@
+import { MatchResponseDTO } from "../api";
 import { EventParser } from "./match.data";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(true);
+describe("EventParser parses a match of", () => {
+  test("soccer", () => {
+    const matchResponse: MatchResponseDTO = {
+      sport: "soccer",
+      participant1: "Chelsea",
+      participant2: "Arsenal",
+      score: "2:1",
+    };
+
+    const parsedMatch = {
+      name: EventParser.makeEventName(matchResponse),
+      score: EventParser.formatScore(matchResponse),
+    };
+
+    expect(parsedMatch).toEqual({
+      name: "Chelsea - Arsenal",
+      score: "2:1",
+    });
+  });
+
+  test("volleyball", () => {
+    const matchResponse: MatchResponseDTO = {
+      sport: "volleyball",
+      participant1: "Germany",
+      participant2: "France",
+      score: "3:0,25:23,25:19,25:21",
+    };
+
+    const parsedMatch = {
+      name: EventParser.makeEventName(matchResponse),
+      score: EventParser.formatScore(matchResponse),
+    };
+
+    expect(parsedMatch).toEqual({
+      name: "Germany - France",
+      score: "Main score: 3:0 (set1 25:23, set2 25:19, set3 25:21)",
+    });
+  });
+
+  test("handball", () => {
+    const matchResponse: MatchResponseDTO = {
+      sport: "handball",
+      participant1: "Pogoń Szczeciń",
+      participant2: "Azoty Puławy",
+      score: "34:26",
+    };
+
+    const parsedMatch = {
+      name: EventParser.makeEventName(matchResponse),
+      score: EventParser.formatScore(matchResponse),
+    };
+
+    expect(parsedMatch).toEqual({
+      name: "Pogoń Szczeciń vs Azoty Puławy",
+      score: "34:26",
+    });
+  });
+
+  test("basketball", () => {
+    const matchResponse: MatchResponseDTO = {
+      sport: "basketball",
+      participant1: "GKS Tychy",
+      participant2: "GKS Katowice",
+      score: [
+        ["9:7", "2:1"],
+        ["5:3", "9:9"],
+      ],
+    };
+
+    const parsedMatch = {
+      name: EventParser.makeEventName(matchResponse),
+      score: EventParser.formatScore(matchResponse),
+    };
+
+    expect(parsedMatch).toEqual({
+      name: "GKS Tychy - GKS Katowice",
+      score: "9:7,2:1,5:3,9:9",
+    });
+  });
+
+  test("tennis", () => {
+    const matchResponse: MatchResponseDTO = {
+      sport: "tennis",
+      participant1: "Maria Sharapova",
+      participant2: "Serena Williams",
+      score: "2:1,7:6,6:3,6:7",
+    };
+
+    const parsedMatch = {
+      name: EventParser.makeEventName(matchResponse),
+      score: EventParser.formatScore(matchResponse),
+    };
+
+    expect(parsedMatch).toEqual({
+      name: "Maria Sharapova vs Serena Williams",
+      score: "Main score: 2:1 (set1 7:6, set2 6:3, set3 6:7)",
+    });
+  });
+
+  test("ski jumping", () => {
+    const matchResponse: MatchResponseDTO = {
+      sport: "ski jumping",
+    };
+
+    const parsedMatch = {
+      name: EventParser.makeEventName(matchResponse),
+      score: EventParser.formatScore(matchResponse),
+    };
+
+    expect(parsedMatch).toEqual({
+      name: "Exception: invalid sport",
+      score: "Exception: invalid sport",
+    });
+  });
 });
